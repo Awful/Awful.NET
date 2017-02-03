5bffc01f1dda9a3d73557d5fb3c0cc01ce822a89
Tim Miller <innerlogic4321@gmail.com>
2017-02-02 22:18:59 -0500

    Add PostIconManager

---
+++ Mazui.Core/Models/Messages/NewPrivateMessage.cs
@@ -0,0 +1,19 @@
+﻿using Mazui.Core.Models.Posts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazui.Core.Models.Messages
+{
+    public class NewPrivateMessage
+    {
+        public PostIcon Icon { get; set; }
+
+        public string Title { get; set; }
+
+        public string Receiver { get; set; }
+
+        public string Body { get; set; }
+    }
+
+}
---
+++ Mazui.Core/Models/PostIcons/PostIconCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mazui.Core.Models.PostIcons
+{
+    public class PostIconCategory
+    {
+        public PostIconCategory(string category, List<PostIcon> list)
+        {
+            List = list;
+            Category = category;
+        }
+
+        public virtual ICollection<PostIcon> List { get; private set; }
+
+        public string Category { get; private set; }
+    }
+}
