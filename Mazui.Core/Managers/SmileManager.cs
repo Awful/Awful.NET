acbc5a1c009e97c2848e5fb6c61906938a05227b
Tim Miller <innerlogic4321@gmail.com>
2017-02-02 22:04:48 -0500

    Add SmileManager

---
+++ Mazui.Core/Managers/SmileManager.cs
@@ -0,0 +1,65 @@
+﻿using HtmlAgilityPack;
+using Mazui.Core.Models.Smilies;
+using Mazui.Core.Models.Web;
+using Mazui.Core.Tools;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazui.Core.Managers
+{
+    public class SmileManager
+    {
+        private readonly WebManager _webManager;
+
+        public SmileManager(WebManager webManager)
+        {
+            _webManager = webManager;
+        }
+
+        public async Task<Result> GetSmileList()
+        {
+            var smileCategoryList = new List<SmileCategory>();
+
+            //inject this
+            var result = await _webManager.GetData(EndPoints.SmileUrl);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(result.ResultHtml);
+            IEnumerable<HtmlNode> smileCategoryTitles =
+                doc.DocumentNode.Descendants("div")
+                    .FirstOrDefault(node => node.GetAttributeValue("class", string.Empty).Contains("inner"))
+                    .Descendants("h3");
+            List<string> categoryTitles =
+                smileCategoryTitles.Select(smileCategoryTitle => WebUtility.HtmlDecode(smileCategoryTitle.InnerText))
+                    .ToList();
+            IEnumerable<HtmlNode> smileNodes =
+                doc.DocumentNode.Descendants("ul")
+                    .Where(node => node.GetAttributeValue("class", string.Empty).Contains("smilie_group"));
+            int smileCount = 0;
+            foreach (HtmlNode smileNode in smileNodes)
+            {
+                var smileList = new List<Smile>();
+                IEnumerable<HtmlNode> smileIcons = smileNode.Descendants("li");
+                foreach (HtmlNode smileIcon in smileIcons)
+                {
+                    var smileEntity = new Smile();
+                    smileEntity.Parse(smileIcon);
+                    smileList.Add(smileEntity);
+                }
+                smileCategoryList.Add(new SmileCategory()
+                {
+                    Name = categoryTitles[smileCount],
+                    SmileList = smileList
+                });
+                smileCount++;
+            }
+            result.ResultJson = JsonConvert.SerializeObject(smileCategoryList);
+            return result;
+        }
+
+    }
+}
---
+++ Mazui.ViewModels/SmiliesViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Template10.Mvvm;
+using Template10.Utils;
+using Mazui.Core.Models.Smilies;
+using Mazui.Core.Managers;
+
+namespace Mazui.ViewModels
+{
+    public class SmiliesViewModel : MazuiViewModel
+    {
+        private ObservableCollection<SmileCategory> _smileCategoryList = new ObservableCollection<SmileCategory>();
+
+        public ObservableCollection<SmileCategory> SmileCategoryList
+        {
+            get { return _smileCategoryList; }
+            set
+            {
+                Set(ref _smileCategoryList, value);
+            }
+        }
+
+        public TextBox ReplyBox { get; set; }
+
+        private bool _isOpen = default(bool);
+
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            set
+            {
+                Set(ref _isOpen, value);
+            }
+        }
+
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                Set(ref _isLoading, value);
+            }
+        }
+
+        private SmileManager _smileManager;
+        public ObservableCollection<SmileCategory> FullSmileCategoryEntities { get; set; }
+
+
+        public async Task LoadSmilies()
+        {
+            if (WebManager == null)
+            {
+                await LoginUser();
+                _smileManager = new SmileManager(WebManager);
+            }
+
+            if (!SmileCategoryList.Any())
+            {
+                IsLoading = true;
+                var result = await _smileManager.GetSmileList();
+                var list = JsonConvert.DeserializeObject<List<SmileCategory>>(result.ResultJson);
+                FullSmileCategoryEntities = list.ToObservableCollection();
+                foreach (var item in list)
+                {
+                    SmileCategoryList.Add(item);
+                }
+                IsLoading = false;
+            }
+        }
+
+        public void SelectIcon(object sender, ItemClickEventArgs e)
+        {
+            var smile = e.ClickedItem as Smile;
+            if (smile == null) return;
+            ReplyBox.Text = ReplyBox.Text.Insert(ReplyBox.Text.Length, smile.Title);
+            IsOpen = false;
+        }
+
+        public void SmiliesFilterOnSuggestedQuery(SearchBox sender, SearchBoxSuggestionsRequestedEventArgs args)
+        {
+            if (SmileCategoryList == null) return;
+            string queryText = args.QueryText;
+            if (string.IsNullOrEmpty(queryText)) return;
+            var suggestionCollection = args.Request.SearchSuggestionCollection;
+            foreach (var smile in SmileCategoryList.SelectMany(smileCategory => smileCategory.SmileList.Where(smile => smile.Title.Contains(queryText))))
+            {
+                suggestionCollection.AppendQuerySuggestion(smile.Title);
+            }
+        }
+
+        public void SmiliesFilterOnSubmittedQuery(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+        {
+            if (SmileCategoryList == null) return;
+            string queryText = args.QueryText;
+            if (string.IsNullOrEmpty(queryText)) return;
+            var result = SmileCategoryList.SelectMany(
+                smileCategory => smileCategory.SmileList.Where(smile => smile.Title.Equals(queryText))).FirstOrDefault();
+            if (result == null)
+            {
+                return;
+            }
+            ReplyBox.Text = ReplyBox.Text.Insert(ReplyBox.Text.Length, result.Title);
+            IsOpen = false;
+        }
+
+        public void SmiliesFilterOnChangedQuery(SearchBox sender, SearchBoxQueryChangedEventArgs args)
+        {
+            string queryText = args.QueryText;
+            if (string.IsNullOrEmpty(queryText))
+            {
+                SmileCategoryList = FullSmileCategoryEntities;
+                return;
+            }
+            var result = SmileCategoryList.SelectMany(
+                smileCategory => smileCategory.SmileList.Where(smile => smile.Title.Equals(queryText))).FirstOrDefault();
+            if (result != null) return;
+            var searchList = FullSmileCategoryEntities.SelectMany(
+                smileCategory => smileCategory.SmileList.Where(smile => smile.Title.Contains(queryText)));
+            List<Smile> smileListEntities = searchList.ToList();
+            var searchSmileCategory = new SmileCategory()
+            {
+                Name = "Search",
+                SmileList = smileListEntities
+            };
+            var fakeSmileCategoryList = new List<SmileCategory> { searchSmileCategory };
+            SmileCategoryList = fakeSmileCategoryList.ToObservableCollection();
+        }
+    }
+}
