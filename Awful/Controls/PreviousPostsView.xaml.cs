ea6d927b540cf69d26c392b8fe6ebac28bc60e61
Tim Miller <innerlogic4321@gmail.com>
2018-08-14 20:30:34 -0400

    Add base previous posts view

---
+++ Awful/Controls/PreviousPostsView.xaml.cs
@@ -0,0 +1,100 @@
+﻿using Awful.Tools;
+using Awful.ViewModels;
+using Awful.Web;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Awful.Controls
+{
+    public sealed partial class PreviousPostsView : UserControl
+    {
+        public static PreviousPostsView Instance { get; set; }
+
+        public PreviousPostsView()
+        {
+            this.InitializeComponent();
+            ViewModel.Web = Web;
+            Instance = this;
+        }
+
+        private async void Web_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            await ViewModel.SetupWebView();
+        }
+
+        private void OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            ViewModel.WebCommands = new WebCommands();
+            sender.AddWebAllowedObject("webCommands", ViewModel.WebCommands);
+        }
+
+        private async void Web_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            try
+            {
+                var test = JsonConvert.DeserializeObject<ForumCommand>(e.Value);
+                await ViewModel.HandleForumCommand(test);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async void Web_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            //await Web.InvokeScriptAsync("app.props.appState.addTestPosts", new string[0]);
+        }
+
+        public async Task LoadBaseView()
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Forums/index.html"));
+            var html = await ParsedHtmlBase(await FileIO.ReadTextAsync(file));
+            Web.NavigateToString(html);
+        }
+
+        private async Task<string> ParsedHtmlBase(string html)
+        {
+            var basePage = await ViewModel.WebManager.Parser.ParseAsync(html);
+            var links = basePage.QuerySelectorAll("link");
+            foreach (var link in links)
+            {
+                var attribute = link.GetAttribute("href");
+                link.SetAttribute("href", $"ms-appx-web:///Assets/Forums{attribute}");
+            }
+            var scripts = basePage.QuerySelectorAll("script");
+            foreach (var script in scripts)
+            {
+                var attribute = script.GetAttribute("src");
+                if (attribute == null)
+                    continue;
+                if (attribute[0] != '/')
+                    script.SetAttribute("src", $"ms-appx-web:///Assets/Forums/{attribute}");
+                else
+                    script.SetAttribute("src", $"ms-appx-web:///Assets/Forums{attribute}");
+            }
+
+            return "<!DOCTYPE html> " + basePage.DocumentElement.OuterHtml;
+        }
+
+        // strongly-typed view models enable x:bind
+        public PreviousPostsViewModel ViewModel => this.DataContext as PreviousPostsViewModel;
+    }
+}
--- Awful/ViewModels/PreviousPostsViewModel.cs
+++ Awful/ViewModels/PreviousPostsViewModel.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using Awful.Parser.Models.Threads;
+using Awful.Tools;
+using Awful.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Awful.ViewModels
 {
-    public class PreviousPostsViewModel
+    public class PreviousPostsViewModel : AwfulViewModel
     {
+        public TextBox ReplyBox { get; set; }
+
+        public WebView Web { get; set; }
+
+        public WebCommands WebCommands { get; set; }
+
+        public async Task SetupWebView()
+        {
+           // await Web.InvokeScriptAsync("FromCSharp", ForumCommandCreator.CreateForumCommand("setupWebview", GetForumThreadSettings()));
+        }
+
+        private bool _isOpen = default(bool);
+
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            set
+            {
+                Set(ref _isOpen, value);
+            }
+        }
+
+        //private ReplyManager _replyManager;
+
+        public async void LoadPreviousPosts(Thread thread, string posts)
+        {
+        //    if (!string.IsNullOrEmpty(PostHtml)) return;
+        //    if (WebManager == null)
+        //    {
+        //        await LoginUser();
+        //    }
+
+        //    _replyManager = new ReplyManager(WebManager);
+        //    var newPosts = JsonConvert.DeserializeObject<List<Post>>(posts);
+            //PostHtml = await HtmlFormater.FormatThreadHtml(thread, newPosts, GetTheme, true, true);
+        }
+
+        public async void AddQuoteString(long postId)
+        {
+            //if (_replyManager == null)
+            //{
+            //    LoginUser();
+            //}
+            //var quoteString = await _replyManager.GetQuoteString(postId);
+            //ReplyBox.Text += Environment.NewLine + quoteString + Environment.NewLine;
+        }
+
+        internal async Task HandleForumCommand(ForumCommand test)
+        {
+            switch (test.Type)
+            {
+                default:
+                    break;
+            }
+        }
     }
 }
