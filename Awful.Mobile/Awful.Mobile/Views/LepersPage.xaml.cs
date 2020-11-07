60b5f6d21ad7e275b9592284097b8d1a19e92074
Tim Miller <innerlogic4321@gmail.com>
2020-11-06 23:32:08 -0500

    Update Themes

---
+++ Awful.Mobile/Awful.Mobile/Views/LepersPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="LepersPage.xaml.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Awful.Mobile.Views
+{
+    /// <summary>
+    /// Lepers Page.
+    /// </summary>
+    public partial class LepersPage : ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LepersPage"/> class.
+        /// </summary>
+        public LepersPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
