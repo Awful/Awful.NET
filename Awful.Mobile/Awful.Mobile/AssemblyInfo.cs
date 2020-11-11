50cb2871ad310ed473c0bdb2edccb02393ca675f
Tim Miller <innerlogic4321@gmail.com>
2020-11-11 11:00:21 -0500

    Fix Fonts

--- Awful.Mobile/Awful.Mobile/AssemblyInfo.cs
+++ Awful.Mobile/Awful.Mobile/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("fa-brands-400.ttf", Alias = "FontAwesomeBrands")]
 [assembly: ExportFont("fa-regular-400.ttf", Alias = "FontAwesomeRegular")]
-[assembly: ExportFont("fa-solid-400.ttf", Alias = "FontAwesomeSolid")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FontAwesomeSolid")]
