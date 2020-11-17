0aba68bf37dc0ccdfa3c0c580602581a75a1658d
Tim Miller <innerlogic4321@gmail.com>
2020-11-17 11:06:22 -0500

    Update files

---
+++ Awful.Mobile/Awful.Mobile/Tools/Converters/BookmarkStarImageConverter.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="BookmarkStarImageConverter.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Awful.Mobile.UI.Tools.Converters
+{
+    public class BookmarkStarImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isFavorited && isFavorited)
+            {
+                return new FontImageSource
+                {
+                    Glyph = "",
+                    FontFamily = "FontAwesomeSolid",
+                    Size = 44
+                };
+            }
+
+            return new FontImageSource
+            {
+                Glyph = "",
+                FontFamily = "FontAwesomeRegular",
+                Size = 44
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
