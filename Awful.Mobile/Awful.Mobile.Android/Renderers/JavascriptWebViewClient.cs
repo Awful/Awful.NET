d40317123c225fed720f29f62bc72fc4d562b3d8
Tim Miller <innerlogic4321@gmail.com>
2020-11-29 12:42:57 -0500

    Switch with hybrid webview

---
+++ Awful.Mobile/Awful.Mobile.Android/Renderers/JavascriptWebViewClient.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="JavascriptWebViewClient.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using Android.Webkit;
+
+namespace Awful.Mobile.Droid.Renderers
+{
+    public class JavascriptWebViewClient : WebViewClient
+    {
+        readonly string _javascript;
+
+        public JavascriptWebViewClient(string javascript)
+        {
+            _javascript = javascript;
+        }
+
+        public override void OnPageFinished(WebView view, string url)
+        {
+            base.OnPageFinished(view, url);
+            view.EvaluateJavascript(_javascript, null);
+        }
+    }
+}
---
+++ Awful.Mobile/Awful.Mobile/Controls/HybridWebView.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="HybridWebView.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Awful.Mobile.Controls
+{
+    /// <summary>
+    /// Hybrid Webview.
+    /// Used to invoke Javascript onto a webpage.
+    /// </summary>
+    public class HybridWebView : WebView
+    {
+        private Action<string> action;
+
+        /// <summary>
+        /// Register Javascript Action.
+        /// </summary>
+        /// <param name="callback">Callback.</param>
+        public void RegisterAction(Action<string> callback)
+        {
+            this.action = callback;
+        }
+
+        /// <summary>
+        /// Cleanup Javascript Action.
+        /// </summary>
+        public void Cleanup()
+        {
+            this.action = null;
+        }
+
+        /// <summary>
+        /// Invoke Action.
+        /// </summary>
+        /// <param name="data">Data used to invoke.</param>
+        public void InvokeAction(string data)
+        {
+            if (this.action == null || data == null)
+            {
+                return;
+            }
+
+            this.action.Invoke(data);
+        }
+    }
+}
