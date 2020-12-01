102d14e5a31a9dc5ced7c52fa877956a7322da14
Tim Miller <innerlogic4321@gmail.com>
2020-11-30 19:35:33 -0500

    Update extras

---
+++ Awful.Mobile/Awful.Mobile/Controls/ExtraPageItem.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ExtraPageItem.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace Awful.Mobile.Controls
+{
+    public class ExtraPageItem
+    {
+        public string Type { get; set; }
+
+        public string Glyph { get; set; }
+
+        public string Title { get; set; }
+    }
+}
--- Awful.Mobile/Awful.Mobile/Pages/ExtraPage.xaml.cs
+++ Awful.Mobile/Awful.Mobile/Pages/ExtraPage.xaml.cs
@@ -4,7 +4,8 @@
 
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using Awful.Mobile.Controls;
 using Xamarin.Forms;
 
 namespace Awful.Mobile.Pages
@@ -16,25 +17,66 @@ namespace Awful.Mobile.Pages
             InitializeComponent();
         }
 
-        async void ImageCell_Tapped(System.Object sender, System.EventArgs e)
+        protected override void OnAppearing()
         {
-            if (sender is Cell cell)
+            base.OnAppearing();
+            this.ExtraPageCollectionView.ItemsSource = new List<ExtraPageItem>()
             {
-                switch (cell.ClassId)
+                new ExtraPageItem()
                 {
-                    case "SAclopediaCell":
-                        await App.PushPageAsync(new SAclopediaEntryListPage()).ConfigureAwait(false);
-                        break;
-                    case "LepersCell":
-                        await App.PushPageAsync(new LepersPage()).ConfigureAwait(false);
-                        break;
-                    case "UserProfileCell":
-                        await App.SetDetailPageAsync(new UserProfilePage(0)).ConfigureAwait(false);
-                        break;
-                    case "AboutCell":
-                        break;
+                    Type = "SAclopediaCell",
+                    Glyph = "",
+                    Title = "SAclopedia",
+                },
+                new ExtraPageItem()
+                {
+                    Type = "LepersCell",
+                    Glyph = "",
+                    Title = "Leper's Colony",
+                },
+                new ExtraPageItem()
+                {
+                    Type = "UserProfileCell",
+                    Glyph = "",
+                    Title = "User Profile",
+                },
+                new ExtraPageItem()
+                {
+                    Type = "AboutCell",
+                    Glyph = "",
+                    Title = "About",
+                },
+            };
+        }
+
+        async void ExtraPageCollectionView_SelectionChanged(System.Object sender, Xamarin.Forms.SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null && e.CurrentSelection.Count > 0)
+            {
+                var selection = e.CurrentSelection[0];
+                if (selection is ExtraPageItem item)
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        switch (item.Type)
+                        {
+                            case "SAclopediaCell":
+                                await App.PushPageAsync(new SAclopediaEntryListPage()).ConfigureAwait(false);
+                                break;
+                            case "LepersCell":
+                                await App.PushPageAsync(new LepersPage()).ConfigureAwait(false);
+                                break;
+                            case "UserProfileCell":
+                                await App.SetDetailPageAsync(new UserProfilePage(0)).ConfigureAwait(false);
+                                break;
+                            case "AboutCell":
+                                break;
+                        }
+                    });
                 }
             }
+
+            this.ExtraPageCollectionView.SelectedItem = null;
         }
     }
 }
