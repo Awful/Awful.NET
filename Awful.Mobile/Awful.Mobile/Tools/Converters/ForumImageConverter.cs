606bf8d164a507a83f7369d261c485026aee0754
Tim Miller <innerlogic4321@gmail.com>
2020-12-01 10:08:11 -0500

    Fix placeholder image

--- Awful.Mobile/Awful.Mobile/Tools/Converters/ForumImageConverter.cs
+++ Awful.Mobile/Awful.Mobile/Tools/Converters/ForumImageConverter.cs
@@ -18,10 +18,26 @@ namespace Awful.Mobile.Tools.Converters
     /// </summary>
     public class ForumImageConverter : IValueConverter
     {
+        private string[] imageNames;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ForumImageConverter"/> class.
+        /// </summary>
+        public ForumImageConverter()
+        {
+            this.imageNames = typeof(ForumImageConverter).GetTypeInfo().Assembly.GetManifestResourceNames();
+        }
+
         /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo language)
         {
-            return ImageSource.FromResource($"Awful.Mobile.ThreadTags.{value}.png", typeof(ForumImageConverter).GetTypeInfo().Assembly);
+            var name = $"Awful.Mobile.ThreadTags.{value}.png";
+            if (this.imageNames.Contains(name))
+            {
+                return ImageSource.FromResource(name, typeof(ForumImageConverter).GetTypeInfo().Assembly);
+            }
+
+            return ImageSource.FromResource($"Awful.Mobile.ThreadTags.missing.png", typeof(ForumImageConverter).GetTypeInfo().Assembly);
         }
 
         /// <inheritdoc/>
