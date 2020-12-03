b729c5cd0515dffc47ff2c6f6adba322f391a818
Tim Miller <innerlogic4321@gmail.com>
2020-12-03 14:19:21 -0500

    Remove SettingsViewModel.cs

--- Awful.Mobile/Awful.Mobile/ViewModels/MobileSettingsPageViewModel.cs
+++ Awful.Mobile/Awful.Mobile/ViewModels/MobileSettingsPageViewModel.cs
@@ -3,10 +3,18 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Awful.Core.Tools;
 using Awful.Database.Context;
+using Awful.Database.Entities;
 using Awful.Mobile.Pages;
+using Awful.UI.Actions;
+using Awful.UI.Tools;
 using Awful.UI.ViewModels;
+using Awful.Webview.Entities.Themes;
+using Microsoft.EntityFrameworkCore;
 using Xamarin.Forms;
 
 namespace Awful.Mobile.ViewModels
@@ -14,8 +22,12 @@ namespace Awful.Mobile.ViewModels
     /// <summary>
     /// Mobile Settings Page View Model.
     /// </summary>
-    public class MobileSettingsPageViewModel : SettingsViewModel
+    public class MobileSettingsPageViewModel : MobileAwfulViewModel
     {
+        private SettingsAction settingActions;
+        private SettingOptions settings;
+        private DeviceColorTheme deviceColorTheme;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MobileSettingsPageViewModel"/> class.
         /// </summary>
@@ -23,16 +35,75 @@ namespace Awful.Mobile.ViewModels
         public MobileSettingsPageViewModel(AwfulContext context)
             : base(context)
         {
+            this.settings = new SettingOptions();
+            this.settingActions = new SettingsAction(context);
+        }
+
+        /// <summary>
+        /// Gets the theme names.
+        /// </summary>
+        public List<string> ThemeNames
+        {
+            get
+            {
+                return Enum.GetNames(typeof(DeviceColorTheme)).Select(b => b).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the device color theme.
+        /// </summary>
+        public DeviceColorTheme DeviceColorTheme
+        {
+            get
+            {
+                return this.deviceColorTheme;
+            }
+
+            set
+            {
+                this.deviceColorTheme = value;
+                if (value != this.settings.DeviceColorTheme)
+                {
+                    this.settings.DeviceColorTheme = value;
+                    Task.Run(async () =>
+                    {
+                        this.settingActions.SetAppTheme(value);
+                        await this.SaveSettingsAsync().ConfigureAwait(false);
+                    });
+                }
+
+                this.OnPropertyChanged(nameof(this.DeviceColorTheme));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to enable background tasks.
+        /// </summary>
+        public bool EnableBackgroundTasks
+        {
+            get
+            {
+                return this.settings.EnableBackgroundTasks;
+            }
+
+            set
+            {
+                this.settings.EnableBackgroundTasks = value;
+                this.OnPropertyChanged(nameof(this.EnableBackgroundTasks));
+                Task.Run(async () => await this.SaveSettingsAsync().ConfigureAwait(false));
+            }
         }
 
         /// <summary>
         /// Gets the login page command.
         /// </summary>
-        public Command LoginPageCommand
+        public AwfulAsyncCommand LoginPageCommand
         {
             get
             {
-                return new Command(async () =>
+                return new AwfulAsyncCommand(
+                    async () =>
                 {
                     if (!this.IsSignedIn)
                     {
@@ -47,8 +118,23 @@ namespace Awful.Mobile.ViewModels
                             Device.BeginInvokeOnMainThread(() => App.Current.MainPage = new LoginPage());
                         }
                     }
-                });
+                },
+                    null,
+                    this);
             }
+        }
+
+        /// <inheritdoc/>
+        public override async Task OnLoad()
+        {
+            await base.OnLoad().ConfigureAwait(false);
+            this.settings = await this.Context.SettingOptionsItems.FirstOrDefaultAsync().ConfigureAwait(false) ?? new SettingOptions();
+            this.DeviceColorTheme = this.settings.DeviceColorTheme;
+        }
+
+        private async Task SaveSettingsAsync()
+        {
+            await this.settingActions.SaveSettingOptionsAsync(this.settings).ConfigureAwait(false);
         }
     }
 }
