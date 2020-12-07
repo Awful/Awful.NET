eaec2bb9c7a5eaee6b92f936c6e414c445d8b45b
Tim Miller <innerlogic4321@gmail.com>
2020-12-07 11:55:34 -0500

    Add Awful Editor for Android

---
+++ Awful.Mobile/Awful.Mobile.Android/Renderers/AwfulEditorRenderer.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="AwfulEditorRenderer.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Awful.Mobile.Controls;
+using Awful.Mobile.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AwfulEditor), typeof(AwfulEditorRenderer))]
+
+namespace Awful.Mobile.Droid.Renderers
+{
+    /// <summary>
+    /// Awful Editor Renderer.
+    /// </summary>
+    public class AwfulEditorRenderer : EditorRenderer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AwfulEditorRenderer"/> class.
+        /// </summary>
+        /// <param name="context">Context.</param>
+        public AwfulEditorRenderer(Context context)
+            : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && e.NewElement is AwfulEditor)
+            {
+                if (this.Control != null)
+                {
+                    this.Control.LayoutChange += this.Control_LayoutChange;
+                }
+            }
+            else
+            {
+                if (this.EditText != null)
+                {
+                    this.Control.LayoutChange -= this.Control_LayoutChange;
+                }
+            }
+        }
+
+        private void Control_LayoutChange(object sender, LayoutChangeEventArgs e)
+        {
+            if (this.Element is AwfulEditor editor && this.Control != null)
+            {
+                int start = this.Control.SelectionStart;
+                int end = this.Control.SelectionEnd;
+                int selectionLength = end - start;
+                if (end > 0 && end > start)
+                {
+                    string selection = this.Control.Text.Substring(start, selectionLength);
+                    editor.SelectedTextStart = start;
+                    editor.SelectedTextEnd = end;
+                    editor.SelectedTextLength = selectionLength;
+                    editor.SelectedText = selection;
+                }
+            }
+        }
+    }
+}
