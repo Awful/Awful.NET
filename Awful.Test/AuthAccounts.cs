4d9d563c27d31a8148a10d4bd41340d31a4a3d36
Tim Miller <innerlogic4321@gmail.com>
2022-01-30 14:06:30 -0500

    Setup Test Auth Accounts

---
+++ Awful.Test/AuthAccounts.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="AuthAccounts.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace Awful.Test
+{
+    /// <summary>
+    /// Auth account usernames and passwords.
+    /// </summary>
+    public static class AuthAccounts
+    {
+        /// <summary>
+        /// Gets the account name.
+        /// </summary>
+        public const string AccountName = "{{AccountName}}";
+
+        /// <summary>
+        /// Gets the account password.
+        /// </summary>
+        public const string AccountPassword = "{{AccountPassword}}";
+
+        /// <summary>
+        /// Gets the plat account name.
+        /// </summary>
+        public const string PlatAccountName = "{{PlatAccountName}}";
+
+        /// <summary>
+        /// Gets the plat account password.
+        /// </summary>
+        public const string PlatAccountPassword = "{{PlatAccountPassword}}";
+
+        /// <summary>
+        /// Gets the banned account name.
+        /// </summary>
+        public const string BannedAccountName = "{{BannedAccountName}}";
+
+        /// <summary>
+        /// Gets the banned account password.
+        /// </summary>
+        public const string BannedAccountPassword = "{{BannedAccountPassword}}";
+    }
+}
