7575f6ea8181ef6213d1fe4181e0a4d84b22a8ca
Tim Miller <innerlogic4321@gmail.com>
2022-01-29 20:26:02 -0500

    Add ITemplateHandler

---
+++ Awful.Core/Themes/DefaultOptions.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="DefaultOptions.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace Awful.Core.Themes
+{
+    /// <summary>
+    /// Default options for themes.
+    /// </summary>
+    public class DefaultOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether we're using dark mode.
+        /// </summary>
+        public bool IsDarkMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether we're using Oled mode.
+        /// </summary>
+        public bool IsOledMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default forum theme id.
+        /// </summary>
+        public int ForumThemeId { get; set; }
+    }
+}
