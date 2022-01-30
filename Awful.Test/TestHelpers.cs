d87e71ecd50296115cce0aeaffd15579738b52dc
Tim Miller <innerlogic4321@gmail.com>
2022-01-30 15:07:19 -0500

    Setup AuthenticationManagerTest.cs

--- Awful.Test/TestHelpers.cs
+++ Awful.Test/TestHelpers.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Drastic Actions. All rights reserved.
 // </copyright>
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Awful.Test
@@ -32,6 +33,62 @@ namespace Awful.Test
             }
 
             return System.IO.File.ReadAllText(filePath);
+        }
+
+        public static (string Username, string Password) GetDefaultAccountInfo()
+        {
+            if (AuthAccounts.AccountName.IsDefaultText())
+            {
+                Assert.Inconclusive("Account name is not set");
+            }
+
+            if (AuthAccounts.AccountPassword.IsDefaultText())
+            {
+                Assert.Inconclusive("Account password is not set");
+            }
+
+            return (AuthAccounts.AccountName, AuthAccounts.AccountPassword);
+        }
+
+        public static (string Username, string Password) GetPlatAccountInfo()
+        {
+            if (AuthAccounts.PlatAccountName.IsDefaultText())
+            {
+                Assert.Inconclusive("Plat Account name is not set");
+            }
+
+            if (AuthAccounts.PlatAccountPassword.IsDefaultText())
+            {
+                Assert.Inconclusive("Plat Account password is not set");
+            }
+
+            return (AuthAccounts.PlatAccountName, AuthAccounts.PlatAccountPassword);
+        }
+
+        public static (string Username, string Password) GetBannedAccountInfo()
+        {
+            if (AuthAccounts.BannedAccountName.IsDefaultText())
+            {
+                Assert.Inconclusive("Banned Account name is not set");
+            }
+
+            if (AuthAccounts.BannedAccountPassword.IsDefaultText())
+            {
+                Assert.Inconclusive("Banned Account password is not set");
+            }
+
+            return (AuthAccounts.BannedAccountName, AuthAccounts.BannedAccountPassword);
+        }
+
+        /// <summary>
+        /// Checks if the text for the account info is the "default" or empty.
+        /// </summary>
+        /// <param name="type">Type.</param>
+        /// <param name="text">Text.</param>
+        /// <returns>Bool.</returns>
+        public static bool IsDefaultText(this string text)
+        {
+            return string.IsNullOrEmpty(text) || text.Contains("{");
         }
     }
 }
